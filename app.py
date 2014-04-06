c2c13fccd2c1356450f11ee2f63ff2d13937c9b6
Evan Allrich <evan@unguku.com>
2014-04-06 00:39:19 -0500

    Remove dead code and tidy up a bit

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-import json
 import platform
 import os
 import time
-import sys
 
-import requests
 from statsd import StatsClient
-
-import settings
 
 host = platform.node()
 
-statsd = StatsClient()
+statsd = StatsClient(prefix=host)
 
-def ns(prefix, key):
-    """Namespace the key beneath the host and the given prefix"""
-    return ".".join([host, prefix, key])
+def ns(*args):
+    """Create a metric key from the given strings"""
+    return ".".join(args)
 
 
 def loadavg():
@@ -26,38 +21,6 @@ def loadavg():
     
     for metric, value in metrics.items():
         statsd.gauge(metric, value)
-    
-    return metrics
-
-
-def _prepare(metrics):
-    now = int(time.time())
-    data = []
-    for metric, value in metrics.items():
-        document = {
-            'metric':    metric,
-            'value':     value,
-            'timestamp': now,
-        }
-        data.append(document)
-    
-    return json.dumps(data)
-
-
-def send(metrics):
-    headers = {
-        'content-type': 'application/json',
-    }
-    payload = _prepare(metrics)
-    
-    for server in settings.servers:
-        try:
-            r = requests.post(server, data=payload, headers=headers)
-            if r.status_code != 201:
-                print "Server %s returned status %d; text:" % (server, r.status_code)
-                print r.text
-        except requests.exceptions.RequestException as exc:
-            print "RequestException against %s: %s" % (server, exc)
 
 
 start = used = 0
@@ -68,9 +31,9 @@ while True:
         loadavg()
     used = int(time.time()) - start
     try:
-        time.sleep(60 - used) # sleep for the remainder of the minute
+        time.sleep(10 - used) # sleep for the remainder of the interval
     except IOError as exc:
-        print "IOError on time.sleep(60 - %r): %s" % (used, exc)
-        # Default sleep
-        time.sleep(55)
+        print "IOError on time.sleep(10 - %r): %s" % (used, exc)
+        # Default sleep after an error
+        time.sleep(8)
 
